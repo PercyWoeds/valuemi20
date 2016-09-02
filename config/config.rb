4d30e33de2d94cae1b4d3f8fc6b3d6a95323b630
percywoeds <percywoeds@gmail.com>
2016-09-02 14:49:18 -0500

    make it better

--- config/config.rb
+++ config/config.rb
@@ -12,13 +12,9 @@ SUNAT.configure do |config|
   config.signature do |s|
     s.party_id    = "20545339006"
     s.party_name  = "CODISCOM S.A.C."
-<<<<<<< HEAD
     s.cert_file   = File.join(Dir.pwd, '/app/keys', 'certificado.crt')
     s.pk_file     = File.join(Dir.pwd, '/app/keys', 'CERTIFICADO.key') 
-=======
-    s.cert_file   = File.join(Dir.pwd, './app/keys', 'certificado.crt')
-    s.pk_file     = File.join(Dir.pwd, './app/keys', 'CERTIFICADO.key') 
->>>>>>> 31270d787f9a3c47fc4406635485a17d24fbbdbf
+    
   end
 
   config.supplier do |s|
@@ -27,11 +23,7 @@ SUNAT.configure do |config|
     s.ruc        = "20545339006"
     s.address_id = "150117"
     s.street     = "CL.IGNACIO TOROTE NRO.515 DPTO 202 URB. EL TREBOL"
-<<<<<<< HEAD
     s.district   = "1RA. ETAPA LOS OLIVOS"
-=======
-    s.district   = " 1RA. ETAPA LOS OLIVOS"
->>>>>>> 31270d787f9a3c47fc4406635485a17d24fbbdbf
     s.city       = "LIMA"
     s.country    = "PE"
     s.logo_path  = "#{Dir.pwd}/app/assets/images/logo.PNG"
