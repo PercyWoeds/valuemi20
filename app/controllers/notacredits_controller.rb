d86764a2aa00334238bf3a80319c88b589c7d73e
percywoeds@gmail.com <percywoeds@gmail.com>
2017-02-28 12:29:27 -0500

    abc

--- app/controllers/notacredits_controller.rb
+++ app/controllers/notacredits_controller.rb
@@ -257,30 +257,19 @@ Banco de CREDITO Cuenta Corriente soles : 191-2231128-0-45 CCI : 002191002231128
         
         credit_note = SUNAT::CreditNote.new(credit_note_data)
         
-        if credit_note.valid?
-          
-
+        if credit_note.valid?          
           credit_note.to_pdf
-
           File::open("credit_note.xml", "w") { |file| file.write(credit_note.to_xml) }
-
-          $lcFileName1= File.expand_path('../../../', __FILE__)+ "/"+$lcFileName        
-          $lcFile2    = File.expand_path('../../../../', __FILE__)+"sunat-ruby9/credit_note.xml"
-        
-        ActionCorreo.bienvenido_email(@invoice).deliver
-    
-        @mailing = Mailing.new(:td =>$lcTd, :serie => 'FF01', :numero => $lcDocument_serial_id, :ruc=>$lcRuc, :flag1 => '1')
-        @mailing.save
-        
-
+          $lcFileName1 = File.expand_path('../../../', __FILE__)+ "/"+$lcFileName        
+          $lcFile2     = File.expand_path('../../../../', __FILE__)+"sunat-ruby9/credit_note.xml"        
+          ActionCorreo.bienvenido_email(@invoice).deliver    
+          @mailing = Mailing.new(:td =>$lcTd, :serie => 'FF01', :numero => $lcDocument_serial_id, :ruc=>$lcRuc, :flag1 => '1')
+          @mailing.save      
         else
-
           $aviso =  "Invalid document, ignoring output: #{credit_note.errors.messages}"
         end
-
         
-        $lcGuiaRemision =""
-             
+        $lcGuiaRemision =""            
 
     end
 
