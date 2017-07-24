d7ab37946e8f2ddb1855a8cb31f29c9074ff1a42
Percy Woeds <percywoeds@gmail.com>
2017-07-24 14:54:30 +0000

    a

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   
   
   
-devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
