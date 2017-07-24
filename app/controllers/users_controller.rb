f254d3537224b2d416ff76c9475591640353b88b
Percy Woeds <percywoeds@gmail.com>
2017-07-24 15:49:54 +0000

    a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,10 +64,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-       if params[:id] = "sign_out"
-        sign_out current_user
-        return
-      end
+       
       @user = User.find(params[:id])
     end
 
