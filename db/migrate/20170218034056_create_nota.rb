9d8f0b13606aed637ec6b8b0a61d0abe28b837a2
Percy Woeds <percywoeds@gmail.com>
2018-01-20 15:09:27 +0000

    a

--- db/migrate/20170218034056_create_nota.rb
+++ db/migrate/20170218034056_create_nota.rb
@@ -1,10 +1,10 @@
 class CreateNota < ActiveRecord::Migration
   def change
-    create_table :nota do |t|
-      t.string :code
-      t.string :descrip
+    # create_table :nota do |t|
+    #   t.string :code
+    #   t.string :descrip
 
-      t.timestamps null: false
-    end
+    #   t.timestamps null: false
+  #  end
   end
 end
