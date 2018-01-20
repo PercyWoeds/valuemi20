f9a837c56afcaf0c52d27aa85648d093ad104939
Percy Woeds <percywoeds@gmail.com>
2018-01-20 15:19:11 +0000

    a

--- db/migrate/20170218162940_add_price2_to_notacredits.rb
+++
@@ -1,5 +0,0 @@
-class AddPrice2ToNotacredits < ActiveRecord::Migration
-  def change
-    add_column :notacredits, :price2, :float
-  end
-end
--- db/migrate/20170303163734_add_notum_id_to_notacredit.rb
+++
@@ -1,5 +0,0 @@
-class AddNotumIdToNotacredit < ActiveRecord::Migration
-  def change
-    add_column :notacredits, :notum_id, :integer
-  end
-end
