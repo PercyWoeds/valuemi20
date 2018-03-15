4a03e74229e84330320f943df0c1a978bd1d057a
Percy Woeds <percywoeds@gmail.com>
2018-03-15 20:19:52 +0000

    a

--- lib/sunat/helpers.rb
+++ lib/sunat/helpers.rb
@@ -5,7 +5,7 @@ module SUNAT
       text = I18n.with_locale(lang) {paymentAmount.int_part.to_words}
       currency = Currency.new(paymentAmount.currency)
       currency_text = currency.plural_name || paymentAmount.currency
-      "#{text} y #{paymentAmount.cents_part.rjust(2,'0')}/100 #{currency_text}"
+      "#{text} y #{paymentAmount.cents_part.to_s.rjust(2,'0')}/100 #{currency_text}"
       
       
     end
